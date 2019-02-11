7c00d907cd8a80e55621f027fc93cfd58a088b0e
Aggregat-V2 <47526686+Aggregat-V2@users.noreply.github.com>
2019-02-11 13:57:21 +0100

    Create auto-trace.sh

---
+++ auto-trace.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# @#Martin Lewald
+
+echo $(date +%Y%m%d_%H%M%S) >> /root/martin-test/run.txt
+
+########################################
+# modify as needed
+########################################
+minfree=15000
+schwellwert=1000
+dumpfilesize=10
+dumphome=/root/auto-trace
+delpcap=10
+
+#######################################
+# dont touch this
+#######################################
+pcaptotal=$(find / -type f -name '*.pcap*' -exec du -c -B MB {} + |grep total$ | awk '{print $1}'| /bin/sed 's/MB//g')
+dfavailable=$(df -B MB | awk '{print $4}' |sed -n 2p| /bin/sed 's/MB//g')
+canuse=$(echo $(($dfavailable+$pcaptotal-$minfree)))
+tcpdumppid=$(pgrep tcpdump |head -n 1)
+minfree2=$(echo $(($minfree+$schwellwert)))
+filenumbers=$(($canuse/$dumpfilesize))
+mkdir -p $dumphome
+chmod 700 $dumphome
+if [ "$dfavailable" -gt "$minfree" ]
+then use=ok
+else
+if pidof tcpdump; then
+kill $tcpdumppid ;find $dumphome -type f -name '*.pcap*' |xargs ls -t |tail -n1 |xargs rm -f ;exit
+else
+   find $dumphome -type f -name '*.pcap*' |xargs ls -t |tail -n1 |xargs rm -f
+fi
+fi
+if [ $dfavailable -lt $minfree2 ]
+then find $dumphome -type f -name '*.pcap*' |xargs ls -t |tail -n1 |xargs rm -f
+else use=ok
+fi
+if pidof tcpdump; then
+exit
+else
+nohup tcpdump -i any host not localhost and port 5060 or port 5061 or portrange 10000-20000 -w "$dumphome/trace.pcap" -s0 -vv -C"$dumpfilesize"M -Zroot -W "$filenumbers" -G -C &
+fi
