8275168443c1d36207fae0c28241dab66ac73a48
Aggregat-V2 <47526686+Aggregat-V2@users.noreply.github.com>
2019-02-11 15:27:47 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 mkdir -p /root/auto-trace && chmod 700 /root/auto-trace
+sleep 2
 wget -O /root/auto-trace/auto-trace.sh https://raw.githubusercontent.com/Aggregat-V2/auto-trace/master/auto-trace.sh && chmod 700 /root/auto-trace/auto-trace.sh
 
 crontab -r
