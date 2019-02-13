a113ec0c8e48dc567bf55254388b25e63e3f5bcc
Aggregat-V2 <47526686+Aggregat-V2@users.noreply.github.com>
2019-02-13 13:03:32 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ sleep 1
 wget -q -O /root/auto-trace/auto-trace.sh https://raw.githubusercontent.com/Aggregat-V2/auto-trace/master/auto-trace.sh
 chmod 700 /root/auto-trace/auto-trace.sh  > /dev/null 2>&1
 killall tcpdump > /dev/null 2>&1
-crontab -r
+crontab -r 2> /dev/null
 cat <(crontab -l) <(echo "SHELL=/bin/bash") |crontab -
 cat <(crontab -l) <(echo "PATH=/usr/local/bin:/usr/local/sbin:/sbin:/usr/sbin:/bin:/usr/bin:/usr/bin/X11") |crontab -
 cat <(crontab -l) <(echo "*/10 * * * * /root/auto-trace/auto-trace.sh > /dev/null 2>&1") | crontab -
